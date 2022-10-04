0667f255d35850543c090c65c4b1a5d7a4085663
yasinyavuz <yavuzyasin.eem@gmail.com>
2022-10-04 14:17:25 +0300

    new variable

--- degisiklik.dart
+++ degisiklik.dart
@@ -1,5 +1,6 @@
 // Yeni bir dal oluşturduk ve onun üzerinde değişiklik yapıyoruz.
 // Commitlemdiğimiz zaman bütün değişiklikler geri alınır.
 void main(){
-
+  // yeni bir değişiklik
+  int a = 10;
 }
